d4c01c87639a928f5c0be425d8badb8e2a3574b8
Ma7en <m9ee9m@gmail.com>
2024-09-09 18:09:24 +0300

    edit vercel

--- app/app.py
+++ app/app.py
@@ -2,4 +2,5 @@ from app import create_app
 
 if __name__ == "__main__":
     app = create_app("prd")
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=app.config["DEBUG"])
--- app/config.py
+++ app/config.py
@@ -19,9 +19,11 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = (
         "postgresql://fayoum:iti@localhost:5432/iti_flask_project_2"
     )
-    SQLALCHEMY_DATABASE_URI = (
+    SQLALCHEMY_DATABASE_URI = "postgres://default:HTpyB9XKVg0m@ep-delicate-mouse-a4xukrvs.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
         "postgres://default:HTpyB9XKVg0m@ep-delicate-mouse-a4xukrvs.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
     )
+
     UPLOADED_PHOTOS_DEST = "app/static/"
 
 
